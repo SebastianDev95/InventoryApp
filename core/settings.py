b96f042c602d5f8a5397ab20a913012eee316ebb
Sebastian <svergaras21@curnvirtual.edu.co>
2025-10-02 21:05:49 -0500

    static files agregados

--- core/settings.py
+++ core/settings.py
@@ -119,6 +119,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",   # para archivos globales
-]
+import os
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"static")),
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
