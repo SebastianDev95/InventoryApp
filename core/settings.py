19d582838a97c642c77543d13402ba51b39279a8
luiswp6 <lsierraa21@curnvirtual.edu.co>
2025-10-11 11:06:30 -0500

    mmm

--- core/settings.py
+++ core/settings.py
@@ -125,4 +125,6 @@ import os
 STATICFILES_DIRS = (os.path.join(BASE_DIR,"static")),
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = '/login'
+
 
--- reportes/urls.py
+++ reportes/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.reportes, name="reportes"),
+    path('', views.reportes, name='reportes'),
+    path('exportar_excel/', views.exportar_excel, name='exportar_excel'),
+    path('exportar_excel_completo/', views.exportar_excel_completo, name='exportar_excel_completo'),
+    path('exportar_pdf/', views.exportar_pdf, name='exportar_pdf'),
+    path('exportar_pdf_completo/', views.exportar_pdf_completo, name='exportar_pdf_completo'),
 ]
