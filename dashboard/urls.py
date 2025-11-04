4ef89b3bdfb6ba40460eb83c0c5eed2195e97f64
Sebastian <svergaras21@curnvirtual.edu.co>
2025-11-04 07:49:45 -0500

    .

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,34 +3,14 @@ from django.urls import path
 from . import views  # Importa tu archivo views.py
 
 urlpatterns = [
-    # --- Autenticación (Nombres ÚNICOS) ---
-    
-    # Ruta de Login (Tus plantillas usan 'login')
     path('login/', views.login_vista, name='login'),
-    
-    # Ruta de Registro (Tus plantillas usan 'register')
     path('register/', views.registro_vista, name='register'),
-    
-    # Ruta de Logout
     path('logout/', views.logout_vista, name='logout'),
-
-    
-    # --- Aplicación Principal ---
-    
     path('dashboard/', views.Dashboard, name='dashboard'),
-
-    
-    # --- CRUD de Productos ---
-    
     path('producto/nuevo/', views.nuevo_producto, name='nuevo_producto'),
     path('producto/editar/<int:pk>/', views.editar_producto, name='editar_producto'),
     path('producto/eliminar/<int:pk>/', views.eliminar_producto, name='eliminar_producto'),
     path('productos/eliminar_seleccionados/', views.eliminar_productos_seleccionados, name='eliminar_productos_seleccionados'),
-
-    
-    # --- Reportes y Perfil ---
-    
-    # Apuntamos temporalmente al dashboard para que los nombres existan
     path('reportes/', views.Dashboard, name='reportes'),
     path('perfil/', views.Dashboard, name='perfil'),
 ]
